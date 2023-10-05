332ce1b22622657e095d9f4208e31b5b68701e3e
Julio Cesar Dias Dos Santos <juliozulu2008@gmail.com>
2023-10-04 23:56:58 -0300

    Inicio do projeto Rede Social

---
+++ 03_projeto_pratico/RedeSocial/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RedeSocial\Controllers;
+
+class HomeController
+{ // Controler da pagina Inicial
+    public function index()
+    {
+        echo "Estou na Home!";
+    }
+}
---
+++ 03_projeto_pratico/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require('vendor/autoload.php'); //Padrao PSR-4
+$app = new RedeSocial\Application();
+
+$app->run();
